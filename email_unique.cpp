c83bcc24ba25e32ecd6e2b093a98d2897f4e3148
Puneet Kumar <pkumar@scu.edu>
2019-01-01 01:47:30 -0800

    Number of unique email

--- email_unique.cpp
+++ email_unique.cpp
@@ -1,7 +1,128 @@
-/* Run time 36 ms
- * Beats 30.59 of cpp submissions
+/* Solution 1 Run time takes 20ms
+ * beats 95.81% of cpp submission
+ */
+
+/* Solution 2 Run time 36 ms
+ * Beats 30.59% of cpp submissions
  */ 
 
+
+/* solution 1 */
+class Solution {
+public:
+    
+    void compute_lps_array(int lps[], string pat){
+
+        int i = 1;
+        int j = 0;
+        lps[0] = 0;
+        int m = pat.length();
+
+        while(i < m){
+                if(pat[i] == pat[j]){
+                        j++;
+                        lps[i] = j;
+                        i++;
+                }
+                else {
+                        if(j != 0){
+                                j = lps[j-1];
+                        }
+                        else {
+                                lps[i] = 0;
+                                i++;
+                        }
+                }
+        }
+
+}
+    
+   int KMPSearch(string pat, string txt){
+
+        int m = pat.length();
+        int n = txt.length();
+        int i = 0;
+        int j = 0;
+
+        int lps[m];
+
+        compute_lps_array(lps, pat);
+
+        while(i < n){
+                if(pat[j] == txt[i]){
+                        i++;
+                        j++;
+                }
+
+                if(j == m){
+                        // cout << "Pattern found at " << i-j << "\n";
+                        return i-j;
+                        j = lps[j-1];
+                }
+                else if(i < n && pat[j] != txt[i]){
+                        if(j != 0){
+                                j = lps[j-1];
+                        } else {
+                                i = i+1;;
+                        }
+                }
+        }
+
+        return -1;
+}
+
+    
+    int numUniqueEmails(vector<string>& emails) {
+        
+       int n = emails.size();
+       int i = 0;
+       // map<string, int> m;
+       unordered_set<string> m;
+       while(i < n){
+         
+           string pattern = "@";
+           string name;
+           string domain;
+           
+           // size_t found = emails[i].find(pattern);
+           int ret = KMPSearch("@", emails[i]);
+           if(ret != -1){
+              domain = emails[i].substr(ret+1); 
+              name = emails[i].substr(0, ret-1);
+           }
+           
+           
+           
+           int len = name.length();
+           int j = 0;
+           string new_name;
+           while(j < len){
+               if(name[j] == '.'){
+                   j++;
+                   continue;
+               }
+               
+               if(name[j] == '+'){
+                   break;
+               }
+               
+               new_name += name[j];
+               j++;
+           }
+           
+           new_name += "@" + domain;
+           //pair<string, int> foo;
+           //foo = make_pair(new_name, i);
+           m.insert(new_name);
+           i++;
+       } 
+        
+        return m.size();
+    }
+};
+
+
+/* Solution 2 */
 class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
