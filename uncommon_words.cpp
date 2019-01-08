d817937b3aaafd464c5eb599354ee84c28ceb914
Puneet Kumar <pkumar@scu.edu>
2019-01-08 03:16:22 -0800

    UNcommon words

---
+++ uncommon_words.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string a, string b) {
+        
+        int len = a.size();
+        map<string, int> m;
+        int i = 0;
+        string temp;
+        vector<string> vec;
+        
+       while(i < len){
+           
+             if(a[i] != ' '){
+                 temp += a[i];
+             } else {
+                 auto it = m.find(temp);
+                 if(it != m.end()){
+                    it->second++;
+                 } else {
+                    pair<string, int> p;
+                    p = make_pair(temp, 1);
+                    m.insert(p); 
+                 }
+                 temp.clear();
+             }   
+            i++;
+       } 
+        
+        if(temp.length() > 0){
+            
+            auto it = m.find(temp);
+            if(it != m.end()){
+                it->second++;
+            } else {
+                pair<string, int> p;
+                p = make_pair(temp, 1);
+                m.insert(p);
+            }
+
+        }
+        
+        temp.clear();
+        len = b.length();
+        i = 0;
+        
+        while(i < len){
+            
+            if(b[i] != ' '){
+                temp += b[i];
+            } else {
+                auto it = m.find(temp);
+                if(it != m.end()){
+                    it->second++;
+                    //cout << "B temp " << temp << "\n";
+                } else {
+                    pair<string, int> p;
+                    p = make_pair(temp, 1);
+                    m.insert(p);
+                }
+                temp.clear();
+            }
+            i++;
+        }
+        
+         if(temp.length() > 0){
+            
+            auto it = m.find(temp);
+            if(it != m.end()){
+                //cout << " extra " << temp;
+                it->second++;
+            } else {
+                pair<string, int> p;
+                p = make_pair(temp, 1);
+                m.insert(p);
+            }
+
+        }
+        
+        
+        for(auto it = m.begin(); it != m.end(); it++){
+            if(it->second < 2)
+                vec.push_back(it->first);
+        }
+       
+        
+        
+        return vec;
+        
+    }
+};
