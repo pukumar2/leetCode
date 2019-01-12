3fa1a433dbcda3ca738823e0ebda2b05585535fd
Puneet Kumar <pkumar@scu.edu>
2019-01-12 13:52:01 -0800

    minimum_height_tree.cpp

---
+++ minimum_height_tree.cpp
@@ -0,0 +1,35 @@
+/* Run time 4ms, beats 97% of CPP Submission */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        
+        if(!root){
+            return 0;
+        }
+        
+       if(root->left == NULL && root->right == NULL){
+           return 1;
+       }
+        
+       if(!root->left){
+           return minDepth(root->right) + 1;
+       }
+        
+       if(!root->right){
+           return minDepth(root->left) + 1;
+       } 
+        
+        return 1 + min(minDepth(root->left), minDepth(root->right));
+        
+    }
+};
