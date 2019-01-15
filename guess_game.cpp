5339c452ae6d23f5a60731a5de06237705887687
Puneet Kumar <pkumar@scu.edu>
2019-01-14 23:51:30 -0800

    Guess game

---
+++ guess_game.cpp
@@ -0,0 +1,29 @@
+/* Run time 0ms and beats 100% of CPP submissions */
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+
+
+       int start = 1;
+        int end = n;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(guess(mid) == -1){
+                end = mid-1;
+            } else if(guess(mid) == 1) {
+                start = mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+
+        return 1;
+    }
+};
