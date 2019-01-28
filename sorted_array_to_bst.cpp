4b450f64738910d13535bce192ac0197259c9891
Puneet Kumar <pkumar@scu.edu>
2019-01-27 22:10:52 -0800

    Sorted array to bst

---
+++ sorted_array_to_bst.cpp
@@ -0,0 +1,60 @@
+/* Run time 4ms and beats 97% CPP Submissions */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode * insert(vector<int> nums, int low, int end){
+
+        int mid = low + (end - low)/2;
+
+        TreeNode *root = new TreeNode(nums[mid]);
+
+        if(low < end){
+
+            if(low < mid){
+                root->left = insert(nums, low, mid-1);
+            }
+            else {
+                root->left = NULL;
+            }
+
+            if(mid < end){
+
+                root->right = insert(nums, mid+1, end);
+            }
+            else {
+                root->right = NULL;
+            }
+
+
+        }
+
+        return root;
+
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+
+        TreeNode *root=NULL;
+        int n = nums.size();
+        if(n == 0){
+            return root;
+        }
+
+        if(n == 1){
+            root =  new TreeNode(nums[0]);
+            return root;
+        }
+
+        return insert(nums, 0, n-1);
+
+    }
+};
