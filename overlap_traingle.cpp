86538101bd43e807f867cda3a2975b375dc81c2c
Puneet Kumar <pkumar@scu.edu>
2019-01-30 00:42:11 -0800

    Overlap traingles

---
+++ overlap_traingle.cpp
@@ -0,0 +1,14 @@
+/* Run time 0ms and beats 100% of CPP Submissions */
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+
+         if(rec1[0] >= rec2[2] || rec1[1] >= rec2[3] || rec2[0] >= rec1[2] ||
+            rec2[1] >= rec1[3]){
+             return false;
+         }
+
+        return true;
+
+    }
+};
