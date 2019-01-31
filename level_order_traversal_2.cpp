680a752861835a406c4d837e4eaeb8f7131188f0
Puneet Kumar <pkumar@scu.edu>
2019-01-31 00:50:32 -0800

    Level order traversal 2

---
+++ level_order_traversal_2.cpp
@@ -0,0 +1,32 @@
+/* Run time 4ms and beats 69% of CPP Submissions */
+class Solution {
+public:
+    bool checkRecord(string s) {
+
+        int num_a = 0;
+        bool late = false;
+        int num_l = 0;
+
+        int n = s.size();
+        if(n == 0){
+            return true;
+        }
+
+        for(int i = 0; i < n; i++){
+
+            if(s[i] == 'A'){
+                num_a++;
+            }
+            if(i+2 < n && s[i] == 'L' && s[i+1] == 'L' && s[i+2] == 'L'){
+                return false;
+            }
+        }
+
+        if(num_a > 1){
+            return false;
+        }
+
+
+        return true;
+    }
+};
