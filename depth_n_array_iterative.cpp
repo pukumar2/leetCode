c54c663986adb84c7af7329ce4ae43f14b65de49
Puneet Kumar <pkumar@scu.edu>
2019-04-14 23:08:33 -0700

    Find depth of a n array tree

--- depth_n_array_iterative.cpp
+++ depth_n_array_iterative.cpp
@@ -1,3 +1,50 @@
+/* Run time 152ms and beats 89% CPP Submissions */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    
+    void helper(Node *root, int& l){
+        
+        if(!root){
+            return;
+        }
+        
+        for(int i = 0; i < root->children.size(); i++){
+            l = max(l, maxDepth(root->children[i]) + 1);
+        }
+        
+    }
+    
+    int maxDepth(Node* root) {
+        
+        if(!root){
+            return 0;
+        }
+        
+        int l = 1;
+        
+        helper(root, l);
+        
+        return l;
+    }
+};
+
+
+
 /*
 // Definition for a Node.
 class Node {
