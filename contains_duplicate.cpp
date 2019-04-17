f55409cc4945e041e5728a91d712fa9f8b3e50f2
Puneet Kumar <pkumar@scu.edu>
2019-04-17 01:23:17 -0700

    Contains duplicate

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -1,3 +1,27 @@
+/* Shortcut */
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+
+        unordered_map<int, int> m;
+
+        for(int i = 0; i < nums.size(); i++){
+            auto it = m.find(nums[i]);
+            if(it != m.end()){
+               if(abs(it->second - i) <= k){
+                   return true;
+               } else {
+                   it->second = i;
+               }
+            } else {
+                m.emplace(nums[i], i);
+            }
+        }
+
+        return false;
+    }
+}; 
+
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
