15866ef91d8f08301b039decbd905d57a50e388e
Puneet Kumar <pkumar@scu.edu>
2019-04-16 20:00:20 -0700

    Modified code

--- sum_of_square_numbers.cpp
+++ sum_of_square_numbers.cpp
@@ -1,3 +1,34 @@
+/* Run time 8ms and beats 60% CPP Submissions */
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+          
+        long top = sqrt(c);
+        long bot = 0;
+        long long sum  = 0;
+        
+        while(bot <= top)
+        {
+            sum = top * top + bot * bot;
+            
+            if(sum == c){
+                return true;
+            }
+            
+            else if(sum > c){
+                top--;
+            }
+            
+            else if(sum < c){
+               bot++;
+            }
+        }
+        
+        return false;
+    }
+};
+
+/* Below code is wrong due to integer overflow */
 // Run time 4ms
 
 class Solution {
