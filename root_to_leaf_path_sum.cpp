8955dfdf3901fc2f7d014c860f09d88daff7200a
Puneet Kumar <pkumar@scu.edu>
2019-04-18 15:46:38 -0700

    Root to leaf path sum

--- root_to_leaf_path_sum.cpp
+++ root_to_leaf_path_sum.cpp
@@ -1,3 +1,47 @@
+// Checked
+// /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool helper(TreeNode *root, int temp_sum, int sum){
+
+         if(!root){
+             return false;
+         }
+
+         if(!root->left && !root->right){
+             if(temp_sum + root->val == sum){
+                 return true;
+             }
+             else {
+                 return false;
+             }
+         }
+
+         temp_sum += root->val;
+
+         return (helper(root->left, temp_sum, sum) || helper(root->right, temp_sum, sum));
+    }
+
+    bool hasPathSum(TreeNode* root, int sum) {
+
+         if(!root){
+             return false;
+         }
+
+         int temp_sum = 0;
+         return helper(root, temp_sum, sum);
+    }
+
+
 /* Run time 8ms and beats 98% of CPP Submissions */
 /**
  * Definition for a binary tree node.
