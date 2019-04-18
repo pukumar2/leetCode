9a6123fd882bd95581685ed4de63472abfdd4f54
Puneet Kumar <pkumar@scu.edu>
2019-04-18 00:37:52 -0700

    Valid parenthesis

--- valid_parenthesis.cpp
+++ valid_parenthesis.cpp
@@ -1,3 +1,44 @@
+/* Run time 4ms and beats 100% CPP Submissions */
+class Solution {
+public:
+    bool isValid(string str) {
+
+        stack<char> s;
+
+        for(auto it : str){
+            if(it == '(' || it == '{' || it == '['){
+                //cout << "push " << it << endl;
+                s.push(it);
+            }
+
+            else if(it == ')'){
+                if(!s.empty() && s.top() == '('){
+                    s.pop();
+                } else {
+                    s.push(it);
+                }
+            }
+            else if(it == '}'){
+                if(!s.empty() && s.top() == '{'){
+                    s.pop();
+                }else {
+                    s.push(it);
+                }
+            }
+            else if(it == ']'){
+                if(!s.empty() && s.top() == '['){
+                    s.pop();
+                }else {
+                    s.push(it);
+                }
+            }
+        }
+
+        return (s.size() == 0);
+    }
+};
+
+
 /* Run time 4ms and beats 90% CPP Submission */
 class Solution {
 public:
