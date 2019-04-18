5c439100e0c1cf881a6a66a14e3519435cb211df
Puneet Kumar <pkumar@scu.edu>
2019-04-18 16:59:46 -0700

    Binary addition

--- binary_addition.cpp
+++ binary_addition.cpp
@@ -56,3 +56,111 @@ public:
         return res;
     }
 };
+
+
+/* Very very long method */
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+        int carry = 0;
+
+        int i = a.length()-1;
+        int j = b.length()-1;
+
+        string res;
+
+        while(i >= 0 && j >= 0){
+
+              if(a[i] == '1' && b[j] == '1' && carry == 0){
+
+                //  cout << "point 1" << endl;
+
+                  carry = 1;
+                  res = "0" + res;
+              }
+            else if(a[i] == '1' && b[j] == '1' && carry == 1){
+                 carry = 1;
+                  res = "1" + res;
+
+            }
+
+              else if((a[i] == '1' || b[j] == '1') && carry == 1){
+                 // cout << "point 2" << endl;
+                  carry = 1;
+                  res = "0" + res;
+              }
+              else if(a[i] == '0' && b[j] == '0' && carry == 1){
+                 // cout << "point 3" << endl;
+                  res = "1" + res;
+                  carry = 0;
+              }
+               else if((a[i] == '1' || b[j] == '1') && carry == 0){
+                 //  cout << "point 4" << endl;
+                  res = "1" + res;
+                  carry = 0;
+               }
+
+               else {
+                //   cout << "point 5" << endl;
+                  res = "0" + res;
+                  carry = 0;
+               }
+
+           //cout << "i: " << i << "j: " << j << res << endl;
+
+            i--;
+            j--;
+
+
+        }
+
+        while( i >= 0){
+
+            if(a[i] == '1' && carry == 1){
+               carry = 1;
+               res = "0" + res;
+            }
+            else if(a[i] == '0' && carry == 1){
+                carry = 0;
+                res = "1" + res;
+            }
+            else if(a[i] == '1' && carry == 0){
+                carry = 0;
+                res = "1" + res;
+            }
+            else{
+                res = "0" + res;
+                carry = 0;
+            }
+            i--;
+        }
+
+        while( j >= 0){
+
+            if(b[j] == '1' && carry == 1){
+               carry = 1;
+               res = "0" + res;
+            }
+            else if(b[j] == '0' && carry == 1){
+                carry = 0;
+                res = "1" + res;
+            }
+            else if(b[j] == '1' && carry == 0){
+                carry = 0;
+                res = "1" + res;
+            }
+            else{
+                res = "0" + res;
+                carry = 0;
+            }
+            j--;
+        }
+
+        if(carry){
+            res = "1" + res;
+        }
+
+        return res;
+    }
+};
