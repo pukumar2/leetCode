51e6cd451d12b721056cd2d41f07f1c401e00fda
Puneet Kumar <pkumar@scu.edu>
2019-04-17 19:34:58 -0700

    Word pattern

--- word_pattern.cpp
+++ word_pattern.cpp
@@ -1,3 +1,67 @@
+/* Run time 4ms and beats 100% CPP Submissions */
+class Solution {
+public:
+    void print_vec(vector<string> res){
+        for(int i = 0; i < res.size(); i++){
+            cout << res[i] << ": " << endl;
+        }
+    }
+
+    bool wordPattern(string pattern, string str) {
+
+        unordered_map<char, string> m;
+        unordered_map<string, char> m2;
+
+        set<char> myset;
+        vector<string> s;
+        istringstream ss(str);
+
+
+        int i = 0;
+        do {
+            string word;
+            ss >> word;
+            if(word.length() > 0)
+                s.push_back(word);
+        } while(ss);
+
+        if(s.size() != pattern.length()){
+            return false;
+        }
+
+        for(int i = 0; i < pattern.size(); i++){
+           // string temp =  + s[i];
+            myset.insert(pattern[i]);
+        }
+
+        int n = s.size();
+        string temp;
+        while(i < n){
+
+            auto it = m.find(pattern[i]);
+            auto itr = m2.find(s[i]);
+            if(it != m.end()){
+                if(it->second != s[i]){
+                    return false;
+                }
+            }
+            else if(itr != m2.end()){
+                if(itr->second != pattern[i]){
+                    return false;
+                }
+            }
+
+            else {
+                m.emplace(pattern[i], s[i]);
+                m2.emplace(s[i], pattern[i]);
+            }
+            i++;
+        }
+
+        return true;
+    }
+};
+
 /* Run time 0ms, beats 100% CPP Submission
  */
 class Solution {
