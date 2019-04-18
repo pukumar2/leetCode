a062ee8eb0910635ed0a80236e1da8a9327e11a3
Puneet Kumar <pkumar@scu.edu>
2019-04-18 00:18:00 -0700

    remove elements from linked list

--- remove_linked_list.cpp
+++ remove_linked_list.cpp
@@ -1,3 +1,45 @@
+/* Run time 28ms and beats 100% CPP SUbmissions */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+
+        ListNode *cur = head;
+
+        ListNode *prev = NULL;
+        while(cur){
+
+            if(cur->val == val){
+               if(prev){
+                  ListNode *next = cur->next;
+                  prev->next = next;
+                  cur = next;
+               } else {
+                  cur = cur->next;
+                  head = cur;
+               }
+
+                continue;
+            }
+
+            prev = cur;
+            cur = cur->next;
+
+        }
+
+        return head;
+
+    }
+};
+
+
 /* Run time 28ms, beats 93% CPP Submission */
 /**
  * Definition for singly-linked list.
