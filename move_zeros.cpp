03c7def6a15104fed371a6484cbfa81ba6146d1f
Puneet Kumar <pkumar@scu.edu>
2019-05-07 23:36:02 -0700

    Move zeros

--- move_zeros.cpp
+++ move_zeros.cpp
@@ -1,4 +1,4 @@
-/* Run time 8ms and beats 100% CPP Submissions, but uses o(n) space */
+/* /* Run time 8ms and beats 100% CPP Submissions, but uses o(n) space */
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -24,51 +24,29 @@ public:
     }
 };
 
-/* Run time 8ms and beats 100% of CPP SUbmissions with O(1) space.*/
+/* Run time 4ms and beats 100% CPP submissions with O(1) space */
 class Solution {
 public:
-    void print_arr(vector<int> nums){
-        int n = nums.size();
-
-        for(int i = 0; i < n; i++){
-            cout << nums[i] << "\t";
-        }
-        cout << "\n";
-    }
-
     void moveZeroes(vector<int>& nums) {
 
-        vector<int> res;
-
         int n = nums.size();
 
-        int i = 0;
-        int j = 0;
+        if(n == 0){
+            return;
+        }
 
-        int flag = 0;
-        while(i < n){
-            if(flag == 0){
-                if(nums[i] != 0){
-                    i++;
-                    j++;
-                }
-                else {
-                    i++;
-                    flag = 1;
-                }
+        int index = 0;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0){
+                nums[index++] = nums[i];
             }
-            else {
-                if(nums[i] != 0){
-                   swap(nums[i], nums[j]);
-                    i++;
-                    j++;
-                }else {
-                    i++;
-                }
-            }
+        }
 
-
+        while(index < n){
+            nums[index++] = 0;
         }
 
     }
 };
+
